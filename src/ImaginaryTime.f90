3e4dd6034e4fe9a4be255b8937e8e9fc40208351
WRyssens <wouter.ryssens@gmail.com>
2016-02-09 13:51:56 +0100

    Fixed a stupid bug in Imaginarytime of the last commit.

--- src/ImaginaryTime.f90
+++ src/ImaginaryTime.f90
@@ -31,16 +31,6 @@ contains
 
     hPsi = NewSpinor()
     hPsi = B + U + W
-    
-    if(B14.ne.0.0_dp .or. B15 .ne. 0.0_dp) then
-        C = ActionOfC(Psi)
-        hPsi = hPsi + C
-    endif
-
-    if(B16.ne.0.0_dp .or. B17 .ne. 0.0_dp) then
-        D = ActionOfD(Psi)
-        hPsi = hPsi + D
-    endif
 
     if(.not.TRC) then
         S = ActionOfS(Psi)
@@ -49,6 +39,16 @@ contains
         hPsi = hPsi + S
         hPsi = hPsi + A
             
+        if(B14.ne.0.0_dp .or. B15 .ne. 0.0_dp) then
+            C = ActionOfC(Psi)
+            hPsi = hPsi + C
+        endif
+
+        if(B16.ne.0.0_dp .or. B17 .ne. 0.0_dp) then
+            D = ActionOfD(Psi)
+            hPsi = hPsi + D
+        endif
+
     endif 
   end function hPsi
 

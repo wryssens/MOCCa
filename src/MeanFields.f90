e1d4e1a615e8e5b4e7f0a19a670e18c096826d64
Wouter Ryssens <w.ryssens@ipnl.in2p3.fr>
2017-03-16 12:43:19 +0100

    more bugfixigin for JPI

--- src/MeanFields.f90
+++ src/MeanFields.f90
@@ -1059,7 +1059,7 @@ contains
         enddo
     enddo
     
-    ActionOfX = MultiplyI(ActionofX)
+    ActionOfX = - MultiplyI(ActionofX)
   end function ActionOfX
 
   function ActionOfA(Psi)
@@ -1420,7 +1420,7 @@ contains
      !-----------------------------------------------------------------------
      ! Action of the Pi field
      !
-     ! + i D_n Fpi_m D_n D_m
+     ! - i D_n Fpi_m D_n D_m
      !-----------------------------------------------------------------------
      type(Spwf), intent(in) :: Psi
      type(Spinor)           :: ActionOfPi, temp, ActionofPiX, ActionOfPiY, ActionOfPiZ
@@ -1449,7 +1449,7 @@ contains
      call DeriveSpinor_Z(temp, ActionOfPiZ,-Psi%Parity, Psi%Signature, Psi%TimeSimplex)
          
      ActionOfPi = ActionOfPiX + ActionOfPiY + ActionOfPiZ
-     ActionOfPi = MultiplyI(ActionOfPi)
+     ActionOfPi = - MultiplyI(ActionOfPi)
      
    end function ActionofPi
 end module MeanFields

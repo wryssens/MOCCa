19372e489f61554e5117f07f17834596de7c0cf1
Wouter Ryssens <w.ryssens@ipnl.in2p3.fr>
2018-02-01 16:08:00 +0100

    New implementation of momentum, corresponding to ideas for toy models.

--- src/ImaginaryTime.f90
+++ src/ImaginaryTime.f90
@@ -45,9 +45,9 @@ contains
    
     integer, intent(in) :: iteration
     
-    integer             :: iter, maxiter, i,j,it,P,S
-    real(KIND=dp)       :: timestep, mu, maxE, dE(nwt), E, mom(nwt), momtot, con
-    real(KIND=dp)       :: minE
+    integer             :: iter, maxiter, i,j,it,P,S, ind
+    real(KIND=dp)       :: timestep, mu, maxE, dE(nwt), E, mom(nwt), con
+    real(KIND=dp)       :: relE, mom_av
     type(Spinor)        :: actionofh, update
     
     if(.not.allocated(mom_estimate)) then
@@ -63,26 +63,16 @@ contains
         return
     endif
 
-    if(Iteration.eq.1) then
+    if(Iteration.eq.1) then 
+        ! Use starting parameters if this is the first iteration.
         dt_estimate  = dt
         mom_estimate = momentum
     endif
     
-    timestep = sum(dt_estimate)/nwt
-    mu       = sum(mom_estimate)/nwt
-    !---------------------------------------------------------------------------
-    ! Step 0: Find minimum energy value on the mesh
-    minE = 1000
-    do i=1,nwt
-        if(HFBasis(i)%energy.lt.minE) minE = HFBasis(i)%energy
-    enddo
-    minE = abs(minE)
-    
     !---------------------------------------------------------------------------
     ! Step 1: evolve the maxspwf in order to estimate the largest eigenvalue 
-    !         on the mesh.
-    !         Do this in a serious way at the first iteration, and only slowly
-    !         for later iterations
+    !         on the mesh. Do this in a serious way at the first iteration,
+    !         and only slowly for later iterations.
     maxiter = 2
     if(Iteration .eq.1) then
         !-----------------------------------------------------------------------
@@ -117,70 +107,53 @@ contains
         ! good enough.
         if(abs(con).lt. 1d-2) exit
     enddo
-    !---------------------------------------------------------------------------
-    ! The time-step is definitely dependent on this estimate of maxE.
-    !timestep = 2/maxE * hbar * 0.95
-
-    !---------------------------------------------------------------------------
-    ! Don't perform fancy updating when momentum is not requested
-    !if(momentum.eq.0.0) return
     
     !---------------------------------------------------------------------------
-    ! Now we estimate for every spwf the necessary momentum
-    momtot= 0
-    de    = 100000
-        
+    ! Step two: for every spwf, find the next highest value.
     do i=1,nwt
-        it = HFBasis(i)%getisospin()
-        P  = HFBasis(i)%getparity()
-        S  = HFBasis(i)%getsignature()
-        E  = HFBasis(i)%energy
-        
-        mom(i) = 0
-        if(HFBasis(i)%occupation.lt.0.1) cycle
-            
+        relE = 10000
+        P =HFBasis(i)%parity 
+        S =HFBasis(i)%signature
+        it=HFBasis(i)%isospin
         do j=1,nwt
-            !-------------------------------------------------------------------
-            ! Loop over all partner wavefunctions.
-            if(i.eq.j) cycle
-            if(HFBasis(j)%isospin.ne.it)  cycle
-            if(HFBasis(j)%parity.ne.P)    cycle
+            if(HFBasis(j)%isospin  .ne.it) cycle
             if(HFBasis(j)%signature.ne.S) cycle
-            
-            ! guard against degeneracies
-            if(abs(E - HFbasis(j)%energy) .gt. 1d-2 ) then
-                ! Find the closest one in the same symmetry block
-                if(abs(E - HFbasis(j)%energy).lt.de(i)) then
-                    de(i) = abs(E - HFbasis(j)%energy)
+            if(HFBasis(j)%parity   .ne.P) cycle
+            ! Notice the safeguard vs accidental degeneracies.
+            if(HFBasis(j)%energy .gt. hfbasis(i)%energy+0.1) then
+                if(hfbasis(j)%energy - hfbasis(i)%energy .lt. relE) then
+                    relE = hfbasis(j)%energy - hfbasis(i)%energy
                 endif
             endif
-            
         enddo
-        mom(i) = ((sqrt(maxE/de(i)) - 1)/(sqrt(maxE/de(i)) + 1))**2
-        momtot= momtot + HFBasis(i)%occupation * mom(i)
-    enddo
-    
-    select case(ParameterEstimation)
-    case(1)
-        ! Optimal value        
-        if(Iteration.eq.1) then
-            dt_estimate  = 2/(maxE + minE) * hbar * 0.95
-        else
-            dt_estimate  = (4/(maxE + minE + 2*sqrt(maxE*minE)) * hbar  * 0.95)
-        endif
-        ! Average value
-        mom_estimate = momtot/(protons+neutrons)
-    case(2)
+        ! Don't use anything if this difference in eigenvalues cannot
+        ! be estimated
+        if(relE .eq. 10000) relE = 0 
 
-    end select
-    print *, '---------------------------------'
-    print 1, 'dt_estimate' , dt_estimate(1)
-    print 1, 'mom_estimate', mom_estimate(1)
-    
-    momtot = momtot/(protons+neutrons)
-    momentum = momtot
-    
-    
+        !-----------------------------------------------------------------------        
+        ! Estimate the best dt
+        if(relE.ne.0) then
+             dt_estimate(i) = 4.0/(maxE - HFBasis(1)%energy + 2*sqrt(maxE))*0.99
+             ! Estimate the best momentum
+             mom_estimate(i) = ((sqrt(maxE) - sqrt(relE))/(sqrt(maxE) + sqrt(relE)))**2
+        else
+             dt_estimate(i) = 2.0/(maxE - HFBasis(1)%energy + 2*sqrt(maxE))
+             mom_estimate(i) = 0.0
+        endif
+    enddo
+
+!    mom_av =  mom_av/(neutrons  + protons)*2
+!    print *, 'average', sum(mom_estimate)/nwt, mom_av
+!    print *, 'dtmax', maxval(dt_estimate)
+!    print *, 'dtmin', minval(dt_estimate)
+
+    ! It is not so bad to overestimate momentum, but really bad to underestimate
+    ! it.
+    mom_estimate = maxval(mom_estimate)
+    mom_av =0
+    do j=1,nwt
+        mom_av = mom_av + hfbasis(j)%occupation * mom_estimate(j)
+    enddo
   end subroutine IterativeEstimation
 
   function hPsi(Psi)
@@ -399,7 +372,6 @@ contains
         if(Current%ConstraintType.eq.0) cycle
     endif
     
-    print *, ' Alternating', current%l, current%m
     select case(Current%Isoswitch)
     case(1)
         !-----------------------------------------------------------------------
@@ -493,7 +465,6 @@ contains
     integer, intent(in)       :: iteration
     type(Spwf)                :: TempWf
     
-    
     !---------------------------------------------------------------------------
     ! Initialization.
     if(.not. allocated(updates)) then
@@ -523,12 +494,14 @@ contains
 
       !-------------------------------------------------------------------------
       ! Add a heavy-ball term and save the update for next time
-      ActionOfH = ActionOfH - SpEnergy * Current + mom_estimate(i) * updates(i)
-      updates(i)= ActionOfH
-    
+!      ActionOfH = ActionOfH  + mom_estimate(i) * updates(i)
+!      updates(i)= ActionOfH
+      ActionOfH = -(dt_estimate(i)/hbar)*ActionofH + mom_estimate(i)*updates(i)
+      ! Save current value to calculate difference
+      updates(i)= current
       !-------------------------------------------------------------------------
       ! Update and save      
-      ActionOfH    =  Current -(dt_estimate(i)/hbar) * ActionOfH
+      ActionOfH = Current + ActionOfH
     
       call HFBasis(i)%SetGrid(ActionOfH)
       call HFBasis(i)%SetEnergy(SpEnergy)
@@ -537,7 +510,11 @@ contains
     !---------------------------------------------------------------------------
     !Orthonormalisation
     call Gramschmidt
-    
+    !---------------------------------------------------------------------------
+    ! Calculate the difference for next time (after orthonormalisation!)
+    do i=1,nwt
+        updates(i) = HFBasis(i)%Value - updates(i)
+    enddo
   end subroutine GradDesc
     
   subroutine Nesterov(Iteration)
@@ -627,6 +604,112 @@ contains
 
 !          ActionOfH = ActionofH - (propfactor * 0.5_dp) * ActionofH2
 !      endif
+!
+!!
+!
+!
+!    !---------------------------------------------------------------------------
+    ! The time-step is definitely dependent on this estimate of maxE.
+    !timestep = 2/maxE * hbar * 0.95
 
+    !---------------------------------------------------------------------------
+    ! Don't perform fancy updating when momentum is not requested
+    !if(momentum.eq.0.0) return
+    
+    !---------------------------------------------------------------------------
+    ! Now we estimate for every spwf the necessary momentum
+!    momtot= 0
+!    de    = 100000
+        
+!    do i=1,nwt
+!        it = HFBasis(i)%getisospin()
+!        P  = HFBasis(i)%getparity()
+!        S  = HFBasis(i)%getsignature()
+!        E  = HFBasis(i)%energy
+!        
+!        mom(i) = 0
+!        if(HFBasis(i)%occupation.lt.0.1) cycle
+!            
+!        do j=1,nwt
+!            !-------------------------------------------------------------------
+!            ! Loop over all partner wavefunctions.
+!            if(i.eq.j) cycle
+!            if(HFBasis(j)%isospin.ne.it)  cycle
+!            if(HFBasis(j)%parity.ne.P)    cycle
+!            if(HFBasis(j)%signature.ne.S) cycle
+!            
+!            ! guard against degeneracies
+!            if(abs(E - HFbasis(j)%energy) .gt. 1d-2 ) then
+!                ! Find the closest one in the same symmetry block
+!                if(abs(E - HFbasis(j)%energy).lt.de(i) .and. HFbasis(j)%energy.gt.E ) then
+!                    de(i) = abs(E - HFbasis(j)%energy)
+!                endif
+!            endif
+!            
+!        enddo
+!        mom(i) = ((sqrt(maxE/de(i)) - 1)/(sqrt(maxE/de(i)) + 1))**2
+!        momtot= momtot + HFBasis(i)%occupation * mom(i)
+!    enddo
+    
+!    select case(ParameterEstimation)
+!    case(1)
+!        ! Optimal value        
+!        if(Iteration.eq.1) then
+!            dt_estimate  = 2/(maxE + minE) * hbar * 0.95
+!        else
+!            dt_estimate  = (4/(maxE + minE + 2*sqrt(maxE*minE)) * hbar  * 0.95)
+!        endif
+!        ! Average value
+!        mom_estimate = momtot/(protons+neutrons)
+!    case(2)
+!       ! Find out the second lowest energy
+!       minE = 1000
+!       do i=1,nwt
+!         if(HFBasis(i)%energy.lt.minE) then
+!            minE = HFBasis(i)%energy
+!            ind  = i
+!         endif
+!       enddo
+!       diffE = 10000
+!       
+!       it = HFBasis(ind)%getisospin()
+!       P  = HFBasis(ind)%getparity()
+!       S  = HFBasis(ind)%getsignature()
+!       print *, it, P,S
+!       do i=1,nwt
+!            if(i.eq.ind) cycle
+!            if(HFBasis(i)%isospin.ne.it)  cycle
+!            if(HFBasis(i)%parity.ne.P)    cycle
+!            if(HFBasis(i)%signature.ne.S) cycle
+!            
+!            if((HFBasis(i)%energy - minE) .lt. diffE) then
+!                diffE = HFBasis(i)%energy
+!            endif
+!       enddo 
+!       print *, diffE
+!       diffE = diffE - minE 
+!       kappa  = (maxE-minE)/diffE
+!       mom_estimate = ((sqrt(kappa) - 1)/(sqrt(kappa) +1 ))
+!       print *, 'Emin', minE, diffE, maxE
+!       print *, 'kappa', kappa, mom_estimate(1)
+!       
+!       if(Iteration.eq.1) then
+!            dt_estimate  = 2/(maxE - minE + diffE) * hbar * 0.95
+!       else
+!            dt_estimate  = 4/(maxE - minE + diffE + 2*sqrt(maxE-minE)*sqrt(diffE)) * hbar * 0.95
+!       endif
+!       
+!       dt_estimate(2:nwt) = dt
+!       print *,'dt',  dt_estimate(1)
+!!       stop
+!    end select
+!    print *, '---------------------------------'
+!    print 1, 'dt_estimate' , dt_estimate(1)
+!    print 1, 'mom_estimate', mom_estimate(1)
+!    print *, 'MaxA', maxE
+!    momtot   = momtot/(protons+neutrons)
+!    momentum = momtot
+!!    stop
+!    
 
 end module Imaginarytime
